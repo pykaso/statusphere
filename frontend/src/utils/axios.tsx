e05276b01e832b06d91bd6446df5dfcd0932cc98
Lukáš Gergel <pykasonet@gmail.com>
2024-09-11 14:50:03 +0200

    docker optimalization

--- frontend/src/utils/axios.tsx
+++ frontend/src/utils/axios.tsx
@@ -4,8 +4,28 @@ import MockAdapter from "axios-mock-adapter";
 // Mock data
 const axiosServices = axios.create({
   baseURL:
-    process.env.NEXT_PUBLIC_REACT_APP_API_URL || "http://10.1.10.145:8888/",
+    process.env.NEXT_PUBLIC_REACT_APP_API_URL || "http://127.0.0.1:8888/",
 });
+
+axiosServices.interceptors.request.use(
+  (request) => {
+    console.error(request);
+    return request;
+  },
+  (error) => {
+    console.error(error);
+  }
+);
+
+axiosServices.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error(error);
+  }
+);
+
 var mock = new MockAdapter(axiosServices);
 
 mock.onAny().passThrough();
